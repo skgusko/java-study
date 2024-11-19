9578dc90c5b9dcb67c99a410df8e95650fe4a153
Konahyeon <koo@inha.edu>
2024-11-19 16:19:20 +0900

    Sol05 edit

--- practice01/src/main/java/prob05/Sol05.java
+++ practice01/src/main/java/prob05/Sol05.java
@@ -3,13 +3,9 @@ package prob05;
 public class Sol05 {
 	public static void main(String[] args) {
 
+		/* 코드 작성 */
 		for (int i = 1; i <= 100; i++) {
-			System.out.println(i);
 			
-			String number = String.valueOf(i);
-			System.out.print(number + " ");
-			System.out.println(number.length());
-			//System.out.println(number.length());
 		}
 	}
 }
